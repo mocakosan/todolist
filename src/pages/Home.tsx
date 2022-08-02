c15e40c78ff0d4870a298eaec4a5d2743a3faf29
mocakosan@naver.com <mocakosan@naver.com>
2022-08-02 16:08:44 +0900

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/home.scss";
+
+const Home = () => {
+  return (
+    <div className="board">
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default Home;
