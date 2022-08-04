f2dfe39608b67ea6c2d6625efec22ac06d60fae1
mocakosan@naver.com <mocakosan@naver.com>
2022-08-04 16:48:26 +0900

    feat:firebase data get

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/home.scss";
 import all from "../all.png";
+import { collection, getDocs, doc, DocumentData } from "firebase/firestore";
+import { db } from "../shared/firebase";
+
+interface SnapshotData {
+  data: DocumentData;
+  id: string;
+}
 
 const Home = () => {
+  const [list, setList] = useState([{}]);
+
+  useEffect(() => {
+    const getList = async () => {
+      const listCollectionRef = collection(db, "todolist");
+      const todoSnap = await getDocs(listCollectionRef);
+      const data: any = todoSnap.docs.map((doc: any) => ({ ...doc.data(), id: doc.id }));
+      setList(data);
+    };
+    getList();
+  }, []);
+  console.log(list);
   return (
     <form className="todo-wrapper">
-      <div className="todo-body">
-        <img src={all} alt="" />
-        <div className="todo-title">
-          <h1>제목</h1>
+      {list.map((item: any) => (
+        <div className="todo-body" key={item.id}>
+          <img src={all} alt="" />
+          <div className="todo-title">
+            <h1>제목</h1>
+            <h1>{item.title}</h1>
+          </div>
+          <div className="todo-content">
+            <h2>오늘의 할일</h2>
+            <h2>{item.content}</h2>
+          </div>
+          <div className="todo-time">
+            <h3>time</h3>
+            <h3>{item.createdAt}</h3>
+          </div>
         </div>
-        <div className="todo-content">
-          <h2>오늘의 할일</h2>
-        </div>
-        <div className="todo-time">
-          <h3>time</h3>
-        </div>
-      </div>
+      ))}
     </form>
   );
 };
