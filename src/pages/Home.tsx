fc84c9d1959216bbdf08187c41d148090a661607
mocakosan@naver.com <mocakosan@naver.com>
2022-08-08 17:26:13 +0900

    fix:delete bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,13 +21,16 @@ const Home = () => {
   console.log(list);
   //데이터 삭제
   const onDelete = async (id: any) => {
-    const todoDoc = doc(db, "todolist", id);
-    await deleteDoc(todoDoc);
+    await deleteDoc(doc(db, "todolist", id));
+    const listCollectionRef = collection(db, "todolist");
+    const todoSnap = await getDocs(listCollectionRef);
+    const data: any = todoSnap.docs.map((doc: any) => ({ ...doc.data(), id: doc.id }));
+    setList(data);
   };
   return (
     <div className="todo-wrapper">
       {list.map((item: any) => (
-        <form className="todo-body" key={item.id}>
+        <div className="todo-body" key={item.id}>
           <div className="todo-title">
             <h1>제목</h1>
             <h1>{item.title}</h1>
@@ -50,7 +53,7 @@ const Home = () => {
           >
             수정
           </button>
-        </form>
+        </div>
       ))}
     </div>
   );
